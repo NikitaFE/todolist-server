31a50da824f81622f38fbe31ffecf5fca9289b06
Nikita Rudenko <mrudenko@gbsfo.com>
2023-07-19 12:06:51 +0300

    Init project, add basic structure

---
+++ src/ormconfig.ts
@@ -0,0 +1,15 @@
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+
+const config: PostgresConnectionOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'todolist',
+  password: '123',
+  database: 'todolist',
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  synchronize: false,
+  migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
+};
+
+export default config;
